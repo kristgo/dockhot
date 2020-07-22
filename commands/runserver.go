f71fede6b7d4b2c6e67ecda5149bc2e4b89b8588
kristgo <intershop58@gmail.com>
2020-07-22 10:44:09 +0300

    release v1

---
+++ commands/runserver.go
@@ -0,0 +1,20 @@
+// Create docker container and push it to Github
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to this life-changing API.\nIts the best API, its true, all other API's are fake.")
+	})
+
+	fmt.Println("Server listening!")
+	http.ListenAndServe(":80", r)
+}
